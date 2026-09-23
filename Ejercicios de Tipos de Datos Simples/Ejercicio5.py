270366ee20be80f6be46ed94812836358c0fde84
Lucas Calderer <lucascalsan@gmail.com>
2026-09-23 13:49:36 +0200

    subida ejercicio 5

---
+++ Ejercicios de Tipos de Datos Simples/Ejercicio5.py
@@ -0,0 +1,7 @@
+horas_trabajadas = float(input("¿Cuántas horas has trabajado? "))
+coste_por_hora = float(input("¿Cuál es el coste por hora? "))
+
+paga = horas_trabajadas * coste_por_hora
+print(f"La paga que te corresponde es: {paga}")
+
+#Un float en Python es un tipo de dato numérico que representa números reales con parte decimal, tanto positivos como negativos
